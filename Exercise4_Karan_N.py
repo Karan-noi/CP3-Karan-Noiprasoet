0ba72b6ac47ae98ee86282529a5438d55d23e900
Karan-noi <83636173+Karan-noi@users.noreply.github.com>
2021-05-04 22:16:58 +0700

    Add files via upload

---
+++ Exercise4_Karan_N.py
@@ -0,0 +1,8 @@
+A =  75
+B =  70
+C =  80
+D =  75
+print("Foundation English : คะแนนที่ได้",A)
+print("General Business : คะแนนที่ได้",B)
+print("Introduction to Computer Systems : คะแนนที่ได้",C)
+print("Computer Programming : คะแนนที่ได้",D)
